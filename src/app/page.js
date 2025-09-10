e73e1aca59340d419c9c288462052ec68fd37445
Dev_Ak <aakash.velu.003@gmail.com>
2025-09-10 20:01:12 +0530

    build the layout

---
+++ src/app/page.js
@@ -0,0 +1,33 @@
+import "../index.css"
+
+export default function App() {
+
+
+  return (
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  )
+}
+
+
+function Logo() {
+  return <h1>Far Away </h1>
+}
+function Form() {
+  return <div className="add-form" > <h3>What do you need for your 😍 trip?</h3></div>
+}
+function PackingList() {
+  return <ul className="list">
+    list
+  </ul>
+}
+function Stats() {
+  return <footer><em>
+    You have X items on your list, and you already packed X (X%)
+
+  </em></footer>
+}
